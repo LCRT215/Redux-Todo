1f9550c9e4dab5683a9668fd464acb2ba7297d94
Leslie Thompson <lcrthompson@gmail.com>
2019-04-30 16:09:23 -0700

    Forgot to do regular commits

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import Todo from "./components/Todo";
 
-function App() {
-  return <div className="App">Testing</div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>hello</h1>
+        <Todo />
+      </div>
+    );
+  }
 }
-
 export default App;
--- todo/src/index.js
+++ todo/src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import "./index.css";
-import { App } from "./components/App";
+import App from "./App";
+
 import { Provider } from "react-redux";
-// import { createStore } from "redux";
+import { createStore } from "redux";
+import { reducers } from "./reducers";
+
+const store = createStore(reducers);
 
 ReactDOM.render(
-  <Provider>
-    {/* store={store} */}
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
